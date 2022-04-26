92fa67089b16ab1f87ada14124aedae1349a8f31
KavithaHT <kavithaht910@gmail.com>
2022-04-26 16:25:26 +0530

    variable and list

---
+++ python_variables/variable.py
@@ -0,0 +1,14 @@
+'''
+python variables:-
+variables are containers for storing data values
+'''
+x=5
+y="kiran"
+print(type(x))
+print(type(y))
+
+print("///////////////////////////////////////////")
+
+x=2
+y=x+5
+print(y)
