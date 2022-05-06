4f4269090621f95c25db8c847e5c438f548756d2
KavithaHT <kavithaht910@gmail.com>
2022-05-06 21:36:33 +0530

    User_input

---
+++ User_input/UIP.py
@@ -0,0 +1,27 @@
+#User input
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+z=x+y
+print(z)
+print("////////////////////////////////////////")
+x=input("Enter the first number:")
+y=input("Enter the second number:")
+z=x+y
+print(z)
+print("///////////////////////////")
+x=str(input("Enter the first name:"))
+y=str(input("Enter the second name:"))
+z=x+" "+y
+print(z)
+print("////////////////////////////////////")
+x=str(input("Enter your GF name:"))
+y=str(input("user GF name is :" +x))
+z=x+y
+print(z)
+#define character
+print("////////////////////////////////////")
+ch=input("Enter the char:")[0]
+print(ch)
+print("////////////////////////////////////")
+result=eval(input("Enter an expr"))
+print(result)
--- python_variables/variable.py
+++ python_variables/variable.py
@@ -1,14 +1,15 @@
 '''
 python variables:-
-variables are containers for storing data values
-'''
-x=5
-y="kiran"
-print(type(x))
-print(type(y))
+variables are containers for storing data valuesstr(input("Enter your lover name:"))print(str(input("Enter your lover name:")))
+
 
 print("///////////////////////////////////////////")
 
 x=2
 y=x+5
 print(y)
+'''
+x=str(input("enter your GF name:"))
+y=str(input("your GF name is:" +x))
+z=x+y
+print(z)
