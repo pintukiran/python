56b2268e069977a05d82f32e0e15c61bbcaa3e30
KavithaHT <kavithaht910@gmail.com>
2022-05-07 07:56:16 +0530

    while_loop

---
+++ while_loop/while_loop.py
@@ -0,0 +1,18 @@
+#while loop
+i=6
+while i>=1:
+    print("rajkumar")
+    i=i-1
+print("//////////////////////////////////")
+i=1
+
+while i<=5:
+    print("pintu", end=" ")
+    j=1
+    while j<=4:
+        print("kiran", end=" ")
+        j=j+1
+
+
+    i=i+1
+    print()
