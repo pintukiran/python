1de8e5601d4acb8c38b6742084a96a88d19a05bd
KavithaHT <kavithaht910@gmail.com>
2022-05-12 07:16:52 +0530

    break_continue_pass

---
+++ for_loop/forloop.py
@@ -0,0 +1,15 @@
+x=["girish","naveen","rajkumar","pavankumar"]
+for i in x:
+    print(x)
+    '''
+output:-
+girish
+naveen
+rajkumar
+pavankumar
+    '''
+    print("///////////////////////////////")
+    y=("support",25,3.45)
+    for i in y:
+        print(i)
+   
